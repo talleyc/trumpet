2c6777af4dd333193da39303ba958358b200e9db
taleyc <christalley2003@yahoo.com>
2012-04-21 22:25:13 -0700

    adding project files

---
+++ pj3/Kruskal.java
@@ -0,0 +1,20 @@
+/* Kruskal.java */
+
+import java.util.Hashtable;
+import graph.*;
+import set.*;
+
+/**
+ * The Kruskal class contains the method minSpanTree(), which implements
+ * Kruskal's algorithm for computing a minimum spanning tree of a graph.
+ */
+
+public class Kruskal {
+
+  /**
+   * minSpanTree() returns a WUGraph that represents the minimum spanning tree
+   * of the WUGraph g.  The original WUGraph g is NOT changed.
+   */
+  public static WUGraph minSpanTree(WUGraph g);
+
+}
